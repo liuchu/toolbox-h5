5d548d03454ba69d41cffcf7ee99b2f23bc0d2c0
liuchu <liuchuu@126.com>
2021-08-21 12:36:42 +0800

    init: 初始化项目

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <p><span>{{msg.summary}}</span></p>
+        <p><span>{{msg.notifyTime}}</span></p>
+        <p><span>{{msg.comment}}</span></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TodoItem",
+        props: {
+            msg: Object
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
